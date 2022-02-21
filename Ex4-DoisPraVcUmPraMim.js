a8e227393dddc98055c59c413a24c14681d42c72
Daniel Andrade <daniel9871994@gmail.com>
2022-02-20 22:21:51 -0400

    Exercício 4 de Java Script

---
+++ Ex4-DoisPraVcUmPraMim.js
@@ -0,0 +1,11 @@
+console.log("Exercício: Um para você e um para mim");
+
+const nomeUm = "Daniel";
+const nomeDois = "Alice";
+const semNome =  "Você";
+const NomeTres = "Carlos";
+
+console.log("Um para",nomeUm+",um para mim.");
+console.log("Um para",nomeDois+",um para mim.");
+console.log("Um para",semNome+",um para mim.");
+console.log("Um para",NomeTres+",outro para mim.");
